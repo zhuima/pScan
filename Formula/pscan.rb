165f1f096ed4aa45632f7200807dfb613cf7b572
zhuima <zhuima314@gmail.com>
2022-04-21 11:22:59 +0800

    Brew formula update for pscan version v1.1.14

--- Formula/pscan.rb
+++ Formula/pscan.rb
@@ -5,20 +5,20 @@
 class Pscan < Formula
   desc "Fast TCP Port Scanner."
   homepage "https://github.com/zhuima/pScan"
-  version "1.1.13"
+  version "1.1.14"
 
   on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/zhuima/pScan/releases/download/v1.1.13/pscan_1.1.13_Darwin_arm64.tar.gz"
-      sha256 "02df1359d49ebefeea6ea1e1f41f0f1b49654aac4ddaf7374e2ced6bbf61016f"
+    if Hardware::CPU.intel?
+      url "https://github.com/zhuima/pScan/releases/download/v1.1.14/pscan_1.1.14_Darwin_x86_64.tar.gz"
+      sha256 "66e846bdd4c7f13bc89c19526b2c7bf5a0e8258f240c389a8465c719ff16aee0"
 
       def install
         bin.install "pscan"
       end
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/zhuima/pScan/releases/download/v1.1.13/pscan_1.1.13_Darwin_x86_64.tar.gz"
-      sha256 "27df8b11bfec402f5ffb6d7e576270133d739d5958e56ca1bee09347ebebf373"
+    if Hardware::CPU.arm?
+      url "https://github.com/zhuima/pScan/releases/download/v1.1.14/pscan_1.1.14_Darwin_arm64.tar.gz"
+      sha256 "6dd564da4668982d9aab780ea9fb2b5763aa458cec51e77e0dcab1df060148a8"
 
       def install
         bin.install "pscan"
@@ -27,17 +27,17 @@ class Pscan < Formula
   end
 
   on_linux do
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/zhuima/pScan/releases/download/v1.1.13/pscan_1.1.13_Linux_arm64.tar.gz"
-      sha256 "f3c0fb1cc48b4c754071d572a418521ac80cbf9dbd8ee175a4afa4bcf6bbff02"
+    if Hardware::CPU.intel?
+      url "https://github.com/zhuima/pScan/releases/download/v1.1.14/pscan_1.1.14_Linux_x86_64.tar.gz"
+      sha256 "70652c958747fbba727241b243566f677c017d88459f902fc64426c2387a3fa4"
 
       def install
         bin.install "pscan"
       end
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/zhuima/pScan/releases/download/v1.1.13/pscan_1.1.13_Linux_x86_64.tar.gz"
-      sha256 "924931a12e1a68ebb94915960d2128e8b5e463ffbf12113c942d828681bcbe59"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/zhuima/pScan/releases/download/v1.1.14/pscan_1.1.14_Linux_arm64.tar.gz"
+      sha256 "de61adb288fb678406ad116d9902e807c4e1834345a4d619fb46e2e70a1213e3"
 
       def install
         bin.install "pscan"
